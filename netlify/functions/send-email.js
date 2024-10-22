e7b7383d6eb8b0524ea3a00e6f2e2193fefb84f6
root <pcabreram@edenorte.com.do>
2024-10-22 19:48:25 -0400

    template for conctact added in template string

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -51,7 +51,79 @@ exports.handler = async (req, context) => {
       // console.log(os.homedir());
       const siteDir = path.join(__dirname, "../../dist"); // Ajusta el nombre de la carpeta si es necesario
       const templateFilePath = path.join(siteDir, "template.html"); // Ruta al archivo dentro de la carpeta 'public'
-      const template = fs.readFileSync(templateFilePath, "utf8");
+      const template = `<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inquietudes o Inconvenientes</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      color: #333;
+      background-color: #f4f4f4;
+      margin: 0;
+      padding: 20px;
+    }
+    .container {
+      max-width: 600px;
+      margin: 0 auto;
+      background-color: #ffffff;
+      padding: 20px;
+      border-radius: 8px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+    h2 {
+      color: #2a2a2a;
+    }
+    p {
+      font-size: 16px;
+      line-height: 1.5;
+    }
+    .message {
+      background-color: #f9f9f9;
+      padding: 15px;
+      border: 1px solid #ddd;
+      border-radius: 5px;
+      margin-top: 20px;
+    }
+    .footer {
+      text-align: center;
+      margin-top: 30px;
+      font-size: 14px;
+      color: #777;
+    }
+    .footer a {
+      color: #1a73e8;
+      text-decoration: none;
+    }
+    .footer a:hover {
+      text-decoration: underline;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h2>Hello <strong>PMMS</strong> Team:</h2>
+    <p>A message has been received from one of our users with the following concerns or issues:</p>
+    
+    <div class="message">
+    <p><strong>User Name:</strong> {{User}} </p>
+    <p><strong>Email:</strong>{{Email}}</p>
+    <p><strong>Subject:</strong>{{Subject}}</p>
+    <p><strong>Message:</strong></p>
+    <p>{{SCHEDULED_MESSAGE}}</p>
+    </div>
+    
+    <p>Please review the message and provide a response as soon as possible.</p>
+    
+    <div class="footer">
+    <p>Thank you for using <strong>[SaaS Name]</strong>. If you need further assistance, you can contact us at <a href="mailto:support@[domain].com">support@[domain].com</a>.</p>
+    </div>
+  </div>
+</body>
+</html>
+`;
       const html = template
         .replace("{{SCHEDULED_MESSAGE}}", content)
         .replace("{{User}}", user)
