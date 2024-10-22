2c6dbe0da60a641277ae6c7a5fe0583a21dc0dd9
root <pcabreram@edenorte.com.do>
2024-10-21 20:08:33 -0400

    sned-email function modified

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -8,75 +8,76 @@ exports.handler = async (req, context) => {
     "Access-Control-Allow-Headers": "*",
     "Access-Control-Allow-Methods": "*",
   };
-  req({
+
+  return {
+    statusCode: 204, // Respuesta vacía, solo con los encabezados CORS
     headers: headers,
-  });
-  console.log(req);
+  };
 
-  try {
-    // Lógica de procesamiento del correo
+  // try {
+  //   // Lógica de procesamiento del correo
 
-    // Configuración del transportador
-    const transporter = nodemailer.createTransport({
-      port: 465,
-      host: "smtp.gmail.com",
-      secure: true,
-      auth: {
-        user: "pcabreram1234@gmail.com",
-        pass: process.env.GOOGLE_APPLICATION_PASSWORD,
-      },
-    });
+  //   // Configuración del transportador
+  //   const transporter = nodemailer.createTransport({
+  //     port: 465,
+  //     host: "smtp.gmail.com",
+  //     secure: true,
+  //     auth: {
+  //       user: "pcabreram1234@gmail.com",
+  //       pass: process.env.GOOGLE_APPLICATION_PASSWORD,
+  //     },
+  //   });
 
-    await transporter.verify();
+  //   await transporter.verify();
 
-    // Leer el template y reemplazar los valores
-    const templatePath = path.join(
-      __dirname,
-      "..",
-      "templates",
-      "mail-template.html"
-    );
-    const template = fs.readFileSync(templatePath, "utf8");
-    const html = template
-      .replace("{{SCHEDULED_MESSAGE}}", req.body.content)
-      .replace("{{User}}", req.body.user)
-      .replace("{{Subject}}", req.body.subject)
-      .replace("{{Email}}", req.body.email);
+  //   // Leer el template y reemplazar los valores
+  //   const templatePath = path.join(
+  //     __dirname,
+  //     "..",
+  //     "templates",
+  //     "mail-template.html"
+  //   );
+  //   const template = fs.readFileSync(templatePath, "utf8");
+  //   const html = template
+  //     .replace("{{SCHEDULED_MESSAGE}}", req.body.content)
+  //     .replace("{{User}}", req.body.user)
+  //     .replace("{{Subject}}", req.body.subject)
+  //     .replace("{{Email}}", req.body.email);
 
-    // Enviar el correo
-    const info = await transporter.sendMail({
-      from: process.env.NODEMAILER_FROM,
-      to: req.body.email,
-      subject: req.body.subject,
-      html,
-    });
+  //   // Enviar el correo
+  //   const info = await transporter.sendMail({
+  //     from: process.env.NODEMAILER_FROM,
+  //     to: req.body.email,
+  //     subject: req.body.subject,
+  //     html,
+  //   });
 
-    const res = new Response(
-      JSON.stringify({
-        message: "Correo enviado exitosamente",
-        messageId: info.messageId,
-      }),
-      {
-        status: 200,
-        headers: headers,
-      }
-    );
+  //   const res = new Response(
+  //     JSON.stringify({
+  //       message: "Correo enviado exitosamente",
+  //       messageId: info.messageId,
+  //     }),
+  //     {
+  //       status: 200,
+  //       headers: headers,
+  //     }
+  //   );
 
-    return res;
-  } catch (error) {
-    console.error("Error al enviar el correo:", error);
+  //   return res;
+  // } catch (error) {
+  //   console.error("Error al enviar el correo:", error);
 
-    const res = new Response(
-      JSON.stringify({
-        error: "Error al procesar la solicitud",
-        details: error.message,
-      }),
-      {
-        status: 500,
-        headers: headers,
-      }
-    );
+  //   const res = new Response(
+  //     JSON.stringify({
+  //       error: "Error al procesar la solicitud",
+  //       details: error.message,
+  //     }),
+  //     {
+  //       status: 500,
+  //       headers: headers,
+  //     }
+  //   );
 
-    return res;
-  }
+  //   return res;
+  // }
 };
