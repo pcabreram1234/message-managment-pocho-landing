8fe96b501f8873f3e435890659007000f34265bd
root <pcabreram@edenorte.com.do>
2024-10-21 20:03:25 -0400

    sned-email function modified

---
+++ netlify/functions/send-email.js
@@ -0,0 +1,82 @@
+const nodemailer = require("nodemailer");
+const fs = require("fs");
+const path = require("path");
+exports.handler = async (req, context) => {
+  const headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Methods": "*",
+  };
+  req({
+    headers: headers,
+  });
+  console.log(req);
+
+  try {
+    // Lógica de procesamiento del correo
+
+    // Configuración del transportador
+    const transporter = nodemailer.createTransport({
+      port: 465,
+      host: "smtp.gmail.com",
+      secure: true,
+      auth: {
+        user: "pcabreram1234@gmail.com",
+        pass: process.env.GOOGLE_APPLICATION_PASSWORD,
+      },
+    });
+
+    await transporter.verify();
+
+    // Leer el template y reemplazar los valores
+    const templatePath = path.join(
+      __dirname,
+      "..",
+      "templates",
+      "mail-template.html"
+    );
+    const template = fs.readFileSync(templatePath, "utf8");
+    const html = template
+      .replace("{{SCHEDULED_MESSAGE}}", req.body.content)
+      .replace("{{User}}", req.body.user)
+      .replace("{{Subject}}", req.body.subject)
+      .replace("{{Email}}", req.body.email);
+
+    // Enviar el correo
+    const info = await transporter.sendMail({
+      from: process.env.NODEMAILER_FROM,
+      to: req.body.email,
+      subject: req.body.subject,
+      html,
+    });
+
+    const res = new Response(
+      JSON.stringify({
+        message: "Correo enviado exitosamente",
+        messageId: info.messageId,
+      }),
+      {
+        status: 200,
+        headers: headers,
+      }
+    );
+
+    return res;
+  } catch (error) {
+    console.error("Error al enviar el correo:", error);
+
+    const res = new Response(
+      JSON.stringify({
+        error: "Error al procesar la solicitud",
+        details: error.message,
+      }),
+      {
+        status: 500,
+        headers: headers,
+      }
+    );
+
+    return res;
+  }
+};
